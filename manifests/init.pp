103279cb530bd6983f55662329ee26c6d9e282bf
Frank Reimer <frank.reimer@kuehne-nagel.com>
2016-04-21 12:05:53 +0200

    Fixed some errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,50 +1,49 @@
 class nmon (
-	$package_rhel7 = 'nmon-rhel7',
-	$package_rhel6 = 'nmon-rhel6'
+        $package_rhel7 = 'nmon-rhel7',
+        $package_rhel6 = 'nmon-rhel6'
 ){
 
 include logrotate
 
 file { '/var/log/nmon':
-	ensure	=> directory
-	}
+        ensure  => directory
+        }
 
 case $operatingsystemmajrelease {
-	'7': {
-		package { $package_rhel7:
-			ensure	=> latest,
-			require	=> File['/var/log/nmon']
-			}
-		}
-	'6': {
-		package { $package_rhel6:
-			ensure	=> latest,
-			require	=> File['/var/log/nmon']
-			}
-		}
-	}
+        '7': {
+                package { $package_rhel7:
+                        ensure  => latest,
+                        require => File['/var/log/nmon']
+                        }
+                }
+        '6': {
+                package { $package_rhel6:
+                        ensure  => latest,
+                        require => File['/var/log/nmon']
+                        }
+                }
+        }
 
 file { '/usr/local/bin/start_nmon.sh':
-	source  => ['puppet:///modules/nmon/usr/local/bin/start_nmon.sh'],
-	owner   => 'root',
-	group   => 'root',
-	mode    => '0755',
-	require	=> Package[$package]
-	}
+        source  => ['puppet:///modules/nmon/usr/local/bin/start_nmon.sh'],
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755'
+        }
 
 cron { start_nmon:
-	command	=> "/usr/local/bin/start_nmon.sh > /dev/null 2>&1",
-	user	=> root,
-	hour	=> 0,
-	minute	=> 0,
-	require	=> File['/usr/local/bin/start_nmon.sh']
-	}
+        command => "/usr/local/bin/start_nmon.sh > /dev/null 2>&1",
+        user    => root,
+        hour    => 0,
+        minute  => 0,
+        require => File['/usr/local/bin/start_nmon.sh']
+        }
 
 logrotate::rule { 'nmon':
-	path		=> '/var/log/nmon/*.nmon',
-	rotate		=> 30,
-	rotate_every	=> 'day',
-	compress	=> true,
-	dateext		=> true
-	}
+        path            => '/var/log/nmon/*.nmon',
+        rotate          => 30,
+        rotate_every    => 'day',
+        compress        => true,
+        dateext         => true
+        }
 }
