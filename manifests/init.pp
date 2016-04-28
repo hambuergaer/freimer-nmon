d630f2c312472685854ebae63a807eb53d8f8abe
Frank Reimer <frank.reimer@kuehne-nagel.com>
2016-04-28 10:34:40 +0200

    Add systemd start file for nmon on RHEL 7, on RHEL 6 we are working with a crontab entry. Furthermore the logrotate configs were adjusted.

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,21 +9,6 @@ file { '/var/log/nmon':
         ensure  => directory
         }
 
-case $operatingsystemmajrelease {
-        '7': {
-                package { $package_rhel7:
-                        ensure  => latest,
-                        require => File['/var/log/nmon']
-                        }
-                }
-        '6': {
-                package { $package_rhel6:
-                        ensure  => latest,
-                        require => File['/var/log/nmon']
-                        }
-                }
-        }
-
 file { '/usr/local/bin/start_nmon.sh':
         source  => ['puppet:///modules/nmon/usr/local/bin/start_nmon.sh'],
         owner   => 'root',
@@ -31,19 +16,65 @@ file { '/usr/local/bin/start_nmon.sh':
         mode    => '0755'
         }
 
-cron { start_nmon:
-        command => "/usr/local/bin/start_nmon.sh > /dev/null 2>&1",
-        user    => root,
-        hour    => 0,
-        minute  => 0,
-        require => File['/usr/local/bin/start_nmon.sh']
+case $operatingsystemmajrelease {
+        '7': {
+                package { $package_rhel7:
+                        ensure  => latest,
+                        require => File['/var/log/nmon']
+                        }
+
+		file { '/etc/systemd/system/nmon.service':
+			ensure	=> present,
+			source	=> ['puppet:///modules/nmon/etc/systemd/system/nmon.service'],
+			owner   => 'root',
+        		group   => 'root',
+        		mode    => '0664',
+			require	=> Package[$package_rhel7]
+			}
+
+		exec { 'reload_systemd':
+			command	=> '/bin/systemctl daemon-reload',
+			subscribe => File['/etc/systemd/system/nmon.service']
+			}
+
+		service { 'nmon':
+        		ensure     => 'running',
+        		enable     => false,
+        		hasstatus  => true,
+       			hasrestart => true,
+			require	   => [ File["/usr/local/bin/start_nmon.sh"], File["/etc/systemd/system/nmon.service"] ]
+        		}
+                }
+        '6': {
+                package { $package_rhel6:
+                        ensure  => latest,
+                        require => File['/var/log/nmon']
+                        }
+		
+		cron { start_nmon:
+        		command => "/usr/local/bin/start_nmon.sh > /dev/null 2>&1",
+        		user    => root,
+        		hour    => 0,
+        		minute  => 1,
+        		require => File['/usr/local/bin/start_nmon.sh']
+        		}
+		
+		logrotate::rule { 'nmon':
+        		path            => '/var/log/nmon/*.nmon',
+        		rotate          => 30,
+        		rotate_every    => 'day',
+        		compress        => true,
+        		dateext         => true,
+			missingok	=> true,
+			ifempty		=> false,
+			prerotate	=> '/bin/pkill nmon > /dev/null 2>&1',
+			postrotate	=> '/usr/local/bin/start_nmon.sh > /dev/null 2>&1'
+        		}
+                }
+
+	default: {
+      		notify {'You are not using RHEL 6/7 or CentOS 6/7': }
+    		}
         }
 
-logrotate::rule { 'nmon':
-        path            => '/var/log/nmon/*.nmon',
-        rotate          => 30,
-        rotate_every    => 'day',
-        compress        => true,
-        dateext         => true
-        }
 }
